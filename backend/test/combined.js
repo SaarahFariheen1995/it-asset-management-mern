93007b70977bd813c1ff0aeca425b8fe087c03cb
Saarah Fariheen Sayeed <n12145521@aut.edu.au>
2025-08-16 02:12:54 +1000

    added test class

---
+++ backend/test/combined.js
@@ -0,0 +1,6 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const http = require('http'); 
+const mongoose = require('mongoose');
+const sinon = require('sinon');
+const { expect } = chai;
