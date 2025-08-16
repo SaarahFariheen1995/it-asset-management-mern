de95baccf6fcbd4dad152bcfe385ee8889323532
Saarah Fariheen Sayeed <n12145521@aut.edu.au>
2025-08-16 16:01:58 +1000

    Updated IP address

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5000', // local
-  baseURL: 'http://3.27.174.126:5001', // live
+  baseURL: 'http://13.54.207.234:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
